ca6a07d6449e878cf61dab25cf66fb68abb85a20
mattwinne <matt.winne@gmail.com>
2022-08-05 16:46:45 -0700

    Improves UI / AAC functionality. Accounts for notch with mobile users

--- src/layout.jsx
+++ src/layout.jsx
@@ -9,7 +9,7 @@ export default function Layout() {
     <>
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=.75, user-scalable=0, maximum-scale=.75, minimum-scale=.75"
+        content="width=device-width, initial-scale=.75, user-scalable=0, maximum-scale=.75, minimum-scale=.75, viewport-fit=cover"
       />
       <div
         style={{
